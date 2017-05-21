3344793e65c1a1553428d4bdbff02f141cae53cc
Dima Ansel <dima.ansel@gmail.com>
2017-05-21 14:27:12 -0500

    Added initialize function & put-side locks.

--- threadsafe_prioqueue/src/threadsafe_prioqueue.h
+++ threadsafe_prioqueue/src/threadsafe_prioqueue.h
@@ -4,35 +4,58 @@
  * The data structure defined in this macro holds only the most recent item of a
  * given type. Types are prioritized by order, but in order to not starve low
  * priority (high index) items, the pull functionality operates in a round robin
- * style.
+ * style. This data structure supports one provider and one consumer.
  * 
  * @date    2017-05-20
  * @author  Noah Ansel
  */
 
 
-#define THREADSAFE_PRIOQUEUE_USED_FLAG 0x1
+#define THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG (char)0x1
+#define THREADSAFE_PRIOQUEUE_LOCKED_FLAG (char)0x2
 
+typedef enum ThreadsafePrioQueueError_enum {
+    THREADSAFE_PRIOQUEUE_OK,
+    THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS,
+    THREADSAFE_PRIOQUEUE_NOT_LOCKED
+} ThreadsafePrioQueueError;
 
 #define THREADSAFE_PRIOQUEUE(NAME, TYPE, SIZE) \
 \
-enum NAME##_error {\
-    /***/\
-    NAME##_OUT_OF_BOUNDS\
-}\
-\
 typedef struct NAME##_Node_struct {\
-    /***/\
-    TYPE data[1];\
-    /***/\
+    TYPE* data;\
     char controlFlag;\
 } NAME##_Node;\
 \
+TYPE NAME##_data[SIZE + 1];\
 NAME##_Node NAME##_nodes[SIZE + 1];\
 \
+int NAME##_init() {\
+    int i;\
+    for (i = 0; i < SIZE + 1; i++) {\
+        NAME##_nodes[i].data = &(NAME##_data[i]);\
+        NAME##_nodes[i].controlFlag = 0;\
+    }\
+}\
+\
 int NAME##_put_ptr(int idx, TYPE** ptr) {\
-    if (idx >= SIZE) {\
+    if (idx < 0 || idx >= SIZE) {\
+        return THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS;\
     }\
     (*ptr) = NAME##_nodes[idx].data;\
-    NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_USED_FLAG;\
+    NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    return THREADSAFE_PRIOQUEUE_OK;\
+}\
+\
+int NAME##_put_unlock(int idx, char occupied) {\
+    if (idx < 0 || idx >= SIZE) {\
+        return THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS;\
+    } else if ((NAME##_nodes[idx].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) == 0) {\
+        return THREADSAFE_PRIOQUEUE_NOT_LOCKED;\
+    }\
+    if (occupied) {\
+        NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG;\
+    }\
+    NAME##_nodes[idx].controlFlag ^= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    return THREADSAFE_PRIOQUEUE_OK;\
 }
--- threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
+++ threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
@@ -32,7 +32,8 @@ THREADSAFE_PRIOQUEUE(TESTSTRUCT, TestStruct, NUM_ELEMENTS)
 class ThreadsafePrioQueueTest : public ::testing::Test {
 protected:
     virtual void SetUp() {
-
+        TESTSTRUCT_init();
+        INTEGER_init();
     }   
 };
 
@@ -41,9 +42,31 @@ TEST_F(ThreadsafePrioQueueTest, put_ptr) {
     TestStruct* data = NULL;
     int* res = NULL;
 
-    TESTSTRUCT_put_ptr(0, &data);
-    INTEGER_put_ptr(30, &res);
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(0, &data));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(30, &res));
 
     EXPECT_FALSE(NULL == data);
     EXPECT_FALSE(NULL == res);
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, TESTSTRUCT_put_ptr(-1, &data));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, INTEGER_put_ptr(35, &res));
+}
+
+TEST_F(ThreadsafePrioQueueTest, put_unlock) {
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_NOT_LOCKED, TESTSTRUCT_put_unlock(2, false));
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, TESTSTRUCT_put_unlock(35, false));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, INTEGER_put_unlock(-1, true));
+
+    TestStruct* data = NULL;
+    int* res = NULL;
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(0, &data));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(30, &res));
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(0, true));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_unlock(30, false));
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG, TESTSTRUCT_nodes[0].controlFlag);
+    EXPECT_EQ(0, INTEGER_nodes[30].controlFlag);
 }
