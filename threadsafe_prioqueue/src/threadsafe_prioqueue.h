cfdf7381df8d7d651f5aee9da7c31e7f91b0ae91
Dima Ansel <dima.ansel@gmail.com>
2017-05-21 09:11:49 -0500

    File & test framework in place.

--- threadsafe_prioqueue/src/threadsafe_prioqueue.h
+++ threadsafe_prioqueue/src/threadsafe_prioqueue.h
@@ -9,3 +9,30 @@
  * @date    2017-05-20
  * @author  Noah Ansel
  */
+
+
+#define THREADSAFE_PRIOQUEUE_USED_FLAG 0x1
+
+
+#define THREADSAFE_PRIOQUEUE(NAME, TYPE, SIZE) \
+\
+enum NAME##_error {\
+    /***/\
+    NAME##_OUT_OF_BOUNDS\
+}\
+\
+typedef struct NAME##_Node_struct {\
+    /***/\
+    TYPE data[1];\
+    /***/\
+    char controlFlag;\
+} NAME##_Node;\
+\
+NAME##_Node NAME##_nodes[SIZE + 1];\
+\
+int NAME##_put_ptr(int idx, TYPE** ptr) {\
+    if (idx >= SIZE) {\
+    }\
+    (*ptr) = NAME##_nodes[idx].data;\
+    NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_USED_FLAG;\
+}
--- threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
+++ threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
@@ -6,3 +6,44 @@
  * @date    2017-05-20
  * @author  Noah Ansel
  */
+
+#include "threadsafe_prioqueue.h"
+#include "gtest/gtest.h"
+
+/**
+ *
+ */
+typedef struct Test_struct {
+    int id;
+    char data[3600];
+} TestStruct;
+
+#define NUM_ELEMENTS 32
+
+// initializes a primitive test queue
+THREADSAFE_PRIOQUEUE(INTEGER, int, NUM_ELEMENTS)
+
+
+// initializes a struct test queue
+THREADSAFE_PRIOQUEUE(TESTSTRUCT, TestStruct, NUM_ELEMENTS)
+
+
+
+class ThreadsafePrioQueueTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {
+
+    }   
+};
+
+
+TEST_F(ThreadsafePrioQueueTest, put_ptr) {
+    TestStruct* data = NULL;
+    int* res = NULL;
+
+    TESTSTRUCT_put_ptr(0, &data);
+    INTEGER_put_ptr(30, &res);
+
+    EXPECT_FALSE(NULL == data);
+    EXPECT_FALSE(NULL == res);
+}
