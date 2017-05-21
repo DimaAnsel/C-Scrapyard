b0073ecc791d903a4dbad318e4733b9c782677bc
Dima Ansel <dima.ansel@gmail.com>
2017-05-21 17:38:47 -0500

    Add pull logic & unit tests.

--- threadsafe_prioqueue/src/threadsafe_prioqueue.h
+++ threadsafe_prioqueue/src/threadsafe_prioqueue.h
@@ -4,58 +4,163 @@
  * The data structure defined in this macro holds only the most recent item of a
  * given type. Types are prioritized by order, but in order to not starve low
  * priority (high index) items, the pull functionality operates in a round robin
- * style. This data structure supports one provider and one consumer.
+ * style. This data structure supports any number of providers and one consumer.
  * 
  * @date    2017-05-20
  * @author  Noah Ansel
  */
 
+#ifndef THREADSAFE_PRIOQUEUE_H_
+#define THREADSAFE_PRIOQUEUE_H_
 
 #define THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG (char)0x1
 #define THREADSAFE_PRIOQUEUE_LOCKED_FLAG (char)0x2
 
+/**
+ * @file
+ * @enum ThreadsafePrioQueueError
+ * Error codes for functions THREADSAFE_PRIOQUEUE()
+ */
 typedef enum ThreadsafePrioQueueError_enum {
     THREADSAFE_PRIOQUEUE_OK,
     THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS,
-    THREADSAFE_PRIOQUEUE_NOT_LOCKED
+    THREADSAFE_PRIOQUEUE_LOCKED,
+    THREADSAFE_PRIOQUEUE_NOT_LOCKED,
+    THREADSAFE_PRIOQUEUE_EMPTY,
+    THREADSAFE_PRIOQUEUE_ERROR
 } ThreadsafePrioQueueError;
 
+/**
+ * Creates a new thread-safe priority queue named NAME that holds
+ * SIZE elements of type TYPE. Defines several methods for accessing the data
+ * structure. 
+ */
 #define THREADSAFE_PRIOQUEUE(NAME, TYPE, SIZE) \
 \
+/*Container for data & metadata.*/\
 typedef struct NAME##_Node_struct {\
+    /*Pointer to data.*/\
     TYPE* data;\
+    /*Bit flags used to mark occupied & locks.*/\
     char controlFlag;\
 } NAME##_Node;\
 \
-TYPE NAME##_data[SIZE + 1];\
-NAME##_Node NAME##_nodes[SIZE + 1];\
+/*Data structure to hold all data for queue.*/\
+typedef struct NAME##_buffer_struct {\
+    /**Queue data.*/\
+    TYPE data[SIZE + 1];\
+    /**Metadata for queue data.*/\
+    NAME##_Node nodes[SIZE + 1];\
+    /**Index of next pull.*/\
+    int pullIdx;\
+} NAME##_buffer;\
 \
-int NAME##_init() {\
+/*Variable storing all data for queue.*/\
+NAME##_buffer NAME;\
+\
+/*Initializes the queue.*/\
+ThreadsafePrioQueueError NAME##_init() {\
     int i;\
+    /*Map all nodes to data*/\
     for (i = 0; i < SIZE + 1; i++) {\
-        NAME##_nodes[i].data = &(NAME##_data[i]);\
-        NAME##_nodes[i].controlFlag = 0;\
+        NAME.nodes[i].data = &(NAME.data[i]);\
+        NAME.nodes[i].controlFlag = 0;\
     }\
-}\
-\
-int NAME##_put_ptr(int idx, TYPE** ptr) {\
-    if (idx < 0 || idx >= SIZE) {\
-        return THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS;\
-    }\
-    (*ptr) = NAME##_nodes[idx].data;\
-    NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    NAME.pullIdx = 0;\
     return THREADSAFE_PRIOQUEUE_OK;\
 }\
 \
-int NAME##_put_unlock(int idx, char occupied) {\
+/*If idx is not locked, updates ptr to location to put data.*/\
+ThreadsafePrioQueueError NAME##_put_ptr(int idx, TYPE** ptr) {\
     if (idx < 0 || idx >= SIZE) {\
+        /*Out of bounds, return error*/\
         return THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS;\
-    } else if ((NAME##_nodes[idx].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) == 0) {\
+    } else if ((NAME.nodes[idx].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) != 0) {\
+        /*idx locked, return error*/\
+        return THREADSAFE_PRIOQUEUE_LOCKED;\
+    }\
+    /*Lock & update ptr, return*/\
+    NAME.nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    (*ptr) = NAME.nodes[idx].data;\
+    return THREADSAFE_PRIOQUEUE_OK;\
+}\
+\
+/*Unlocks the specified idx and updates the occupied flag.*/\
+ThreadsafePrioQueueError NAME##_put_unlock(int idx, char occupied) {\
+    if (idx < 0 || idx >= SIZE) {\
+        /*Out of bounds, return error*/\
+        return THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS;\
+    } else if ((NAME.nodes[idx].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) == 0) {\
+        /*Not locked, return error*/\
         return THREADSAFE_PRIOQUEUE_NOT_LOCKED;\
     }\
+    /*Update occupied flag*/\
     if (occupied) {\
-        NAME##_nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG;\
+        NAME.nodes[idx].controlFlag |= THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG;\
     }\
-    NAME##_nodes[idx].controlFlag ^= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    /*Unlock & return*/\
+    NAME.nodes[idx].controlFlag ^= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
     return THREADSAFE_PRIOQUEUE_OK;\
+}\
+\
+/*If pull is not locked, updates ptr to location to pull data from.*/\
+ThreadsafePrioQueueError NAME##_pull_ptr(TYPE** ptr) {\
+    if ((NAME.nodes[SIZE].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) != 0) {\
+        /*Pull locked, return error*/\
+        return THREADSAFE_PRIOQUEUE_LOCKED;\
+    }\
+    /*Get pullIdx now for thread safety.*/\
+    int i, firstIdx = NAME.pullIdx;\
+    TYPE* temp;\
+    /*Begin searching from pullIdx.*/\
+    for (i = firstIdx;;) {\
+        if (((NAME.nodes[i].controlFlag & THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG) != 0) &&\
+                ((NAME.nodes[i].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) == 0)) {\
+            /*Found an unlocked occupied data slot, atomically swap data and mark as unoccupied*/\
+            /*Lock*/\
+            NAME.nodes[i].controlFlag |= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+            NAME.nodes[SIZE].controlFlag |= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+            /*Swap*/\
+            temp = NAME.nodes[SIZE].data;\
+            NAME.nodes[SIZE].data = NAME.nodes[i].data;\
+            NAME.nodes[i].data = temp;\
+            /*Unlock, free*/\
+            NAME.nodes[i].controlFlag ^= (THREADSAFE_PRIOQUEUE_LOCKED_FLAG | \
+                THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG);\
+            /*Update ptr & pullIdx, return OK*/\
+            NAME.pullIdx = (i + 1) % SIZE;\
+            (*ptr) = NAME.nodes[SIZE].data;\
+            return THREADSAFE_PRIOQUEUE_OK;\
+        }\
+        /*Increment, wrap, check if completed cycle.*/\
+        i++;\
+        if (i == SIZE) {\
+            i = 0;\
+        }\
+        if (i == firstIdx) {\
+            /*Reset pullIdx, return error*/\
+            NAME.pullIdx = 0;\
+            return THREADSAFE_PRIOQUEUE_EMPTY;\
+        }\
+    }\
+    /*Unknown error occurred*/\
+    return THREADSAFE_PRIOQUEUE_ERROR;\
+}\
+\
+/*Unlocks the pull data.*/\
+ThreadsafePrioQueueError NAME##_pull_unlock() {\
+    if ((NAME.nodes[SIZE].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG) == 0) {\
+        /*Pull not locked, return error*/\
+        return THREADSAFE_PRIOQUEUE_NOT_LOCKED;\
+    }\
+    /*Unlock & return*/\
+    NAME.nodes[SIZE].controlFlag ^= THREADSAFE_PRIOQUEUE_LOCKED_FLAG;\
+    return THREADSAFE_PRIOQUEUE_OK;\
+}\
+\
+/*Resets pullIdx to 0. Future pulls will begin from 0 (highest priority).*/\
+ThreadsafePrioQueueError NAME##_reset_pullIdx() {\
+    NAME.pullIdx = 0;\
 }
+
+#endif // THREADSAFE_PRIOQUEUE_H_
--- threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
+++ threadsafe_prioqueue/test/threadsafe_prioqueue_test.cc
@@ -41,12 +41,16 @@ protected:
 TEST_F(ThreadsafePrioQueueTest, put_ptr) {
     TestStruct* data = NULL;
     int* res = NULL;
+    int* res2 = NULL;
 
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(0, &data));
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(30, &res));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_LOCKED, INTEGER_put_ptr(30, &res2));
+    EXPECT_EQ(TESTSTRUCT.nodes[0].data, data);
+    EXPECT_EQ(INTEGER.nodes[30].data, res);
 
-    EXPECT_FALSE(NULL == data);
-    EXPECT_FALSE(NULL == res);
+    // EXPECT_FALSE(NULL == data);
+    // EXPECT_FALSE(NULL == res);
 
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, TESTSTRUCT_put_ptr(-1, &data));
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OUT_OF_BOUNDS, INTEGER_put_ptr(35, &res));
@@ -60,13 +64,94 @@ TEST_F(ThreadsafePrioQueueTest, put_unlock) {
 
     TestStruct* data = NULL;
     int* res = NULL;
+    int* res2 = NULL;
 
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(0, &data));
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(30, &res));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_LOCKED, INTEGER_put_ptr(30, &res2));
 
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(0, true));
     EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_unlock(30, false));
 
-    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG, TESTSTRUCT_nodes[0].controlFlag);
-    EXPECT_EQ(0, INTEGER_nodes[30].controlFlag);
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OCCUPIED_FLAG, TESTSTRUCT.nodes[0].controlFlag);
+    EXPECT_EQ(0, INTEGER.nodes[30].controlFlag);
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(30, &res2));
+}
+
+TEST_F(ThreadsafePrioQueueTest, pull_ptr) {
+    TestStruct* dataIn = NULL;
+    TestStruct* dataOut = NULL;
+
+    // pull when empty
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_EMPTY, TESTSTRUCT_pull_ptr(&dataOut));
+
+    // pull when written but empty
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(10, &dataIn));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(10, false));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_EMPTY, TESTSTRUCT_pull_ptr(&dataOut));
+
+    // pull when non-empty but locked
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(4, &dataIn));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_EMPTY, TESTSTRUCT_pull_ptr(&dataOut));
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(4, true));
+
+    // pull when non-empty & not locked
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_pull_ptr(&dataOut));
+    EXPECT_FALSE(dataOut == NULL);
+    EXPECT_EQ(dataIn, dataOut);
+
+    // pull when pull locked
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_LOCKED, TESTSTRUCT_pull_ptr(&dataOut));
+
+    // unlock
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_pull_unlock());
+
+    // pull with need to wrap
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(1, &dataIn));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(1, true));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(4, &dataOut));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(4, true));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_pull_ptr(&dataOut));
+    EXPECT_FALSE(dataOut == NULL);
+    EXPECT_EQ(dataIn, dataOut);
+}
+
+TEST_F(ThreadsafePrioQueueTest, pull_unlock) {
+    TestStruct* dataIn = NULL;
+    TestStruct* dataOut = NULL;
+
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_NOT_LOCKED, TESTSTRUCT_pull_unlock());
+
+    // put data
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_ptr(1, &dataIn));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_put_unlock(1, true));
+
+    // pull
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_pull_ptr(&dataOut));
+
+    // check lock flag
+    EXPECT_NE(0, TESTSTRUCT.nodes[32].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG);
+
+    // unlock
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, TESTSTRUCT_pull_unlock());
+
+    // check lock flag
+    EXPECT_EQ(0, TESTSTRUCT.nodes[32].controlFlag & THREADSAFE_PRIOQUEUE_LOCKED_FLAG);
+}
+
+TEST_F(ThreadsafePrioQueueTest, reset_pullIdx) {
+    int* dataIn = NULL;
+    int* dataOut = NULL;
+
+    // put data & pull
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_ptr(3, &dataIn));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_put_unlock(3, true));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_pull_ptr(&dataOut));
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_pull_unlock());
+    
+    EXPECT_EQ(4, INTEGER.pullIdx);
+    EXPECT_EQ(THREADSAFE_PRIOQUEUE_OK, INTEGER_reset_pullIdx());
+    EXPECT_EQ(0, INTEGER.pullIdx);
 }
